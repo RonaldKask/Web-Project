3a9a42e8dde54ccf498a0b5acc2421dff242addf
Opilane TTHK <opilane@tthk.ee>
2020-11-11 14:26:14 +0200

    js

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+const currentImage = document.querySelector("#image");
+const images = ["img/pic1.jpg", "img/pic2.jpg", "img/pic3.jpg", "img/pic4.jpg"];
+
 function changeimage(){
     console.log("Hello from script file");
+    let rnd = Math.floor(Math.random() * images.length);
+    currentImage.src = images[rnd];
+    console.log(rnd);
+
 }
